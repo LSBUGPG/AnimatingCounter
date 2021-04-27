ad0d0744776da2ab856706d8b3021dfec6e8bb59
Paul Sinnett <paul.sinnett@gmail.com>
2021-04-27 13:48:43 +0100

    Initial commit

---
+++ Assets/CountUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUp : MonoBehaviour
+{
+    public int score;
+    public float duration = 5f;
+    Text scoreText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    [ContextMenu("Play")]
+    public void Play()
+    {
+        if (Application.isPlaying)
+        {
+            StartCoroutine(Animate(score));
+        }
+        else
+        {
+            Debug.Log("You need to be running");
+        }
+    }
+
+    IEnumerator Animate(int target)
+    {
+        float t = 0;
+        if (target > 0)
+        {
+            while (t < 1f)
+            {
+                scoreText.text = (target - (target / Mathf.Pow(target, t))).ToString("N0");
+                t += Time.deltaTime / duration;
+                yield return null;
+            }
+        }
+        scoreText.text = target.ToString("N0");
+    }
+}
